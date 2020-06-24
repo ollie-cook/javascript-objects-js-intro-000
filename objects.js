1cb6ef371d7b8be40b32b36ee0ec20e7bacd02aa
Oliver Cook <olliewatford@gmail.com>
2020-06-24 12:36:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
 }
 
 var updatePlaylist = (playlistName, artistName, songTitle) => {
-return Object.assign({},playlistName,{[artistName]:'songTitle'})
+return Object.assign(playlistName,{[artistName]:'songTitle'})
 
 }

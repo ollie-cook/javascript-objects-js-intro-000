c2999f12878076546e13fea54fd1bb8adba3b909
Oliver Cook <olliewatford@gmail.com>
2020-06-24 12:43:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,5 +4,9 @@ var playlist = {
 
 var updatePlaylist = (playlistName, artistName, songTitle) => {
 return Object.assign(playlistName,{[artistName]:'songTitle'})
+}
 
+var removeFromPlaylist = (playlistName, artistName) => {
+  delete playlistName.artistName;
+  return playlistName
 }
